4cecb1e2413e0828561ef5d9c777320e2b9b2588
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HelloGrid/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using DevExpress.Mobile.DataGrid;
+
+namespace HelloGrid
+{	
+	public partial class MainPage : ContentPage
+	{	
+		int count;
+		public MainPage ()
+		{
+			InitializeComponent ();
+
+			TestOrdersRepository model = new TestOrdersRepository ();
+			BindingContext = model;
+		}
+
+        #region #CustomSummary
+        void OnCalculateCustomSummary(object sender, CustomSummaryEventArgs e){
+			if (e.FieldName.ToString () == "Shipped")
+			if (e.IsTotalSummary){
+				if (e.SummaryProcess == CustomSummaryProcess.Start) {
+					count = 0;
+				}
+				if (e.SummaryProcess == CustomSummaryProcess.Calculate) {
+					if (!(bool)e.FieldValue)
+						count++;
+					e.TotalValue = count;
+				}
+			}
+		}
+        #endregion #CustomSummary
+
+        #region #CustomCell
+        void OnCustomizeCell(CustomizeCellEventArgs e)
+        {
+            if (e.FieldName == "Total" && !e.IsSelected)
+            {
+                int total = Convert.ToInt32(e.Value);
+                if (total < 50)
+                    e.ForeColor = Color.Red;
+                else if (total > 2000)
+                    e.ForeColor = Color.Green;
+                e.Handled = true;
+            }
+        }
+        #endregion #CustomCell
+
+        #region #SwipeButtons
+        void OnSwipeButtonShowing(object sender, SwipeButtonShowingEventArgs e)
+        {
+            if ((!(Boolean)grid.GetCellValue(e.RowHandle, "Shipped"))
+                && (e.ButtonInfo.ButtonName == "RightButton"))
+            {
+                e.IsVisible = false;
+            }
+        }
+
+        void OnSwipeButtonClick(object sender, SwipeButtonEventArgs e)
+        {
+            if (e.ButtonInfo.ButtonName == "LeftButton")
+            {
+                DateTime orderDate = (DateTime)grid.GetCellValue(e.RowHandle, "Date");
+                string orderDateDay = orderDate.ToString("dddd");
+                DisplayAlert("Alert from " + e.ButtonInfo.ButtonName, "Day: " + orderDateDay, "OK");
+            }
+            if (e.ButtonInfo.ButtonName == "RightButton")
+            {
+                grid.DeleteRow(e.RowHandle);
+            }
+        }
+        #endregion #SwipeButtons
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Grid for Xamarin - Getting Started
+
+
+<p>This example is created by following instructions of the <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12265">Getting Started</a> tutorial. It demonstrates the base functionality of the <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12264">DevExpress GridControl for Xamarin.Forms</a> and includes the following steps:<br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12266">Bind Grid to Data</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12267">Create Grid Columns</a> <br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12268">Add New Item Row</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12269">Group Data</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12270">Calculate Data Summaries</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12271">Sort Data</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12272">Filter Data</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12277">Customize Cell Appearance</a><br />- <a href="https://documentation.devexpress.com/#Xamarin/CustomDocument12278">Implement Swipe Buttons for Data Rows</a></p>
+
+<br/>
+
+
